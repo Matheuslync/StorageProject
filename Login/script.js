35c4cf5588fbf06e64c01c8c1d269015b227ec27
Matheuslync <146727207+Matheuslync@users.noreply.github.com>
2025-05-12 09:27:38 -0300

    correção do login

--- Login/script.js
+++ Login/script.js
@@ -1,22 +1,21 @@
-// show password
 document.addEventListener("DOMContentLoaded", () => {
     const passwordInput = document.getElementById('password');
     const eyeIcon = document.querySelector('.bi-eye-fill');
 
     eyeIcon.addEventListener('click', () => {
         const isPassword = passwordInput.type === 'password';
-
         passwordInput.type = isPassword ? 'text' : 'password';
 
         eyeIcon.classList.toggle('bi-eye-fill');
         eyeIcon.classList.toggle('bi-eye-slash-fill');
     });
 
+    document.getElementById("login").addEventListener("click", login); 
 });
 
 function login(){
-    var nome = $("#username").val()
-    var senha = $("#password").val()
+    var nome = $("#username").val();
+    var senha = $("#password").val();
 
     if(nome && senha && nome === "admin" && senha === "12345"){
         const user = {
@@ -25,18 +24,18 @@ function login(){
             id: Math.floor(Math.random() * 100000),
         };
 
-        localStorage.setItem("usuario", JSON.stringify(user))
+        localStorage.setItem("usuario", JSON.stringify(user));
 
-        window.location.href = "../Loja/index.html"
+        window.location.href = "/loja/index.html";
     }else{
-        document.getElementById("error-modal").style.display = "flex"
-        document.getElementById("nome").style.borderBottom = "3px solid red"
-        document.getElementById("senha").style.borderBottom = "3px solid red"
+        document.getElementById("error-modal").style.display = "flex";
+        document.getElementById("username").style.borderBottom = "3px solid red";
+        document.getElementById("password").style.borderBottom = "3px solid red";
     }
 }
 
 function fecharError(){
-    document.getElementById("error-modal").style.display = "none"
-    document.getElementById("nome").style.borderBottom = " 2px solid #FFECD1"
-    document.getElementById("senha").style.borderBottom = " 2px solid #FFECD1"
+    document.getElementById("error-modal").style.display = "none";
+    document.getElementById("username").style.borderBottom = "2px solid #FFECD1";
+    document.getElementById("password").style.borderBottom = "2px solid #FFECD1";
 }
